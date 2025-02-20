2b3520f665c6c8f23f8a9c33a60baf67d6004017
jupyterj0nes <88971898+jupyterj0nes@users.noreply.github.com>
2025-02-20 09:54:27 +0000

    Recursive zip analysis for EVTX extraction. Debug mode. New parser for ELK exported logs in winlogbeat format

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
-
 use masstin::Cli;
 use clap::Parser;
 use std::process;
 use tokio;
 
 #[tokio::main]
-async fn  main() {
+async fn main() {
     let cli = Cli::parse();
 
     if let Err(e) = masstin::run(cli).await {
